3a683b2a4d94cde971d74a619942c0da0105a77e
Gurya26 <anamgurya2626@gmail.com>
2026-05-13 11:08:51 +0530

    Final Django Task Manager Project

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -1,22 +1,16 @@
 from pathlib import Path
 
-import os
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-your-secret-key'
-
+SECRET_KEY = 'django-secret-12345'
 
 DEBUG = True
-
 
 ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -25,28 +19,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'tasks',
-
 ]
 
 
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.csrf.CsrfViewMiddleware',
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 
@@ -54,7 +40,6 @@ ROOT_URLCONF = 'taskmanager.urls'
 
 
 TEMPLATES = [
-
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
@@ -63,21 +48,13 @@ TEMPLATES = [
         'APP_DIRS': True,
 
         'OPTIONS': {
-
             'context_processors': [
-
                 'django.template.context_processors.request',
-
                 'django.contrib.auth.context_processors.auth',
-
                 'django.contrib.messages.context_processors.messages',
-
             ],
-
         },
-
     },
-
 ]
 
 
@@ -85,64 +62,48 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 
 
 DATABASES = {
-
     'default': {
-
         'ENGINE': 'django.db.backends.sqlite3',
-
         'NAME': BASE_DIR / 'db.sqlite3',
-
     }
-
 }
 
 
 AUTH_PASSWORD_VALIDATORS = [
-
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-
 ]
 
 
 LANGUAGE_CODE = 'en-us'
 
-
 TIME_ZONE = 'UTC'
 
-
 USE_I18N = True
-
 
 USE_TZ = True
 
 
 STATIC_URL = 'static/'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = '/login/'
+LOGIN_URL = 'login'
 
+LOGIN_REDIRECT_URL = 'home'
 
-LOGIN_REDIRECT_URL = '/'
-
-
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = 'login'
